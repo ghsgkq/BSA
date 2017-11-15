dbdb78fcc5a1cc6f7e0d59c40eed2be643ee8a00
ghsgkq <ckdgus5820@gmail.com>
2017-11-15 23:03:41 +0900

    2017-11-15 23:03
    아이디 중복확인 80% 완성

--- BSA_JSP/BSA/src/BSA/action/idCheckAction.java
+++ BSA_JSP/BSA/src/BSA/action/idCheckAction.java
@@ -1,13 +1,32 @@
 package BSA.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import BSA.model.memberDao;
 
 public class idCheckAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest req, HttpServletResponse resp) throws Throwable {
-		System.out.println(req.getParameter("id"));
+		String id = req.getParameter("id");
+		List<String> ids = memberDao.getInstance().memberGetId();
+		int idcheck = 1;
+		
+		if(ids != null) {
+			for(int i=0; i<ids.size(); i++) {
+				if(id.equals(ids.get(i))) {
+					idcheck=0;
+				}
+			}
+		}
+		
+		if(idcheck == 1) {
+			req.setAttribute("id", id);
+		}
+		req.setAttribute("idcheck", idcheck);
 		return "/JSP/register.jsp";
 	}
 	
