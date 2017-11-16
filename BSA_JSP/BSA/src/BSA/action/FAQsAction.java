b60d38ffc14741b0a11f7d1f34374f988b3a882b
yunchea1014 <yuncha1014@naver.com>
2017-11-16 14:08:57 +0900

    top 수정중
    top 수정중

---
+++ BSA_JSP/BSA/src/BSA/action/FAQsAction.java
@@ -0,0 +1,13 @@
+package BSA.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FAQsAction implements CommandAction{
+
+	@Override
+	public String requestPro(HttpServletRequest req, HttpServletResponse resp) throws Throwable {
+		return "/JSP/FAQs.jsp";
+	}
+	
+}
