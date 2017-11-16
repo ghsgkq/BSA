9395fbff8422192a92e22073b7cac1f5d3bf7bb6
kendong18 <kendong18@gmail.com>
2017-11-16 09:44:22 +0900

    NonMemberDao
    DB

---
+++ BSA_JSP/BSA/src/BSA/model/NonMemberDao.java
@@ -0,0 +1,47 @@
+package BSA.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class NonMemberDao {
+	private static NonMemberDao instance = null;
+	private NonMemberDao() {}
+	public static NonMemberDao getInstance() {
+		if(instance == null) {
+			synchronized (NonMemberDao.class) {
+				instance = new NonMemberDao();
+			}
+		}
+		return instance;
+	}
+	
+	public void NonMemberinsert(NonMemberDto article) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = ConnUtil.getConnection();
+			pstmt = conn.prepareStatement("insert into NONMEMBER (FIRST_NAME, "
+					+ "LAST_NAME, PHONE_NUMBER, EMAIL_ADDRESS, COMMENT)"
+					+ "vlaue(NONMEMBER_SEQ.nextval, ?, ?, ?, ?, ?)");
+			pstmt.setString(1, article.getFirstname());
+			pstmt.setString(2, article.getLastname());
+			pstmt.setString(3, article.getPhonenum());
+			pstmt.setString(4, article.getEmail());
+			pstmt.setString(5, article.getComment());
+			pstmt.executeUpdate();
+		}catch(SQLException e) {
+				e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null)pstmt.close();
+				if(conn != null)pstmt.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
