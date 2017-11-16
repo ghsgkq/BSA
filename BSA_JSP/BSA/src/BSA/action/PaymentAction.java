14cfb6ede491a40f784414604fd06ea554c3a7be
kendong18 <kendong18@gmail.com>
2017-11-16 10:06:17 +0900

    PaymentAction, payment_pro
    payment_pro -> ??.do 요부분 어떻게 해야 할지 모르겠네요

---
+++ BSA_JSP/BSA/src/BSA/action/PaymentAction.java
@@ -0,0 +1,26 @@
+package BSA.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import BSA.model.NonMemberDao;
+import BSA.model.NonMemberDto;
+
+public class PaymentAction implements CommandAction{
+	
+		public String requestPro(HttpServletRequest request, HttpServletResponse response)throws Throwable{
+			request.setCharacterEncoding("UTF-8");
+			NonMemberDto article = new NonMemberDto();
+			article.setFirstname(request.getParameter("firstname"));
+			article.setLastname(request.getParameter("lastname"));
+			article.setPhonenum(request.getParameter("phonenum"));
+			article.setEmail(request.getParameter("email"));
+			article.setComment(request.getParameter("comment"));
+			
+			NonMemberDao dbPro = NonMemberDao.getInstance();
+			dbPro.NonMemberinsert(article);
+			
+			return "/JSP/payment_pro.jsp";
+			
+		}
+}
