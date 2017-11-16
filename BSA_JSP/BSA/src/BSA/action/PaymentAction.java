5634a19767442a0685c6342b5f81befb9ba6f3b0
kendong18 <kendong18@gmail.com>
2017-11-16 14:15:10 +0900

    어쨌든 paymentaction2
    잘 모르겠네여

--- BSA_JSP/BSA/src/BSA/action/PaymentAction.java
+++ BSA_JSP/BSA/src/BSA/action/PaymentAction.java
@@ -3,6 +3,8 @@ package BSA.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import BSA.model.CreditCardDao;
+import BSA.model.CreditCardDto;
 import BSA.model.NonMemberDao;
 import BSA.model.NonMemberDto;
 
@@ -10,16 +12,18 @@ public class PaymentAction implements CommandAction{
 	
 		public String requestPro(HttpServletRequest request, HttpServletResponse response)throws Throwable{
 			request.setCharacterEncoding("UTF-8");
+			
 			NonMemberDto article = new NonMemberDto();
+			
 			article.setFirst_name(request.getParameter("first_name"));
 			article.setLast_name(request.getParameter("last_name"));
 			article.setPhone_number(request.getParameter("phone_number"));
 			article.setEmail_address(request.getParameter("email_address"));
 			article.setComment(request.getParameter("comment"));
-			
+
 			NonMemberDao dbPro = NonMemberDao.getInstance();
 			dbPro.NonMemberinsert(article);
-			
+
 			return "/JSP/payment.jsp";
 			
 		}
