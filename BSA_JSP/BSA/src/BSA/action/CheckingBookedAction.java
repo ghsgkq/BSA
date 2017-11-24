fe2206a95fdc9f7ddf767d42e73cee15336b3e86
ghsgkq <ckdgus5820@gmail.com>
2017-11-24 13:15:22 +0900

    BookingDao 함수 추가
    getBooking 추가

--- BSA_JSP/BSA/src/BSA/action/CheckingBookedAction.java
+++ BSA_JSP/BSA/src/BSA/action/CheckingBookedAction.java
@@ -3,11 +3,16 @@ package BSA.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import BSA.model.memberDto;
+
 public class CheckingBookedAction implements CommandAction{
 
 	@Override
 	public String requestPro(HttpServletRequest req, HttpServletResponse resp) throws Throwable {
-		// TODO Auto-generated method stub
+		if(req.getSession().getAttribute("id") != null) {
+			
+		}
+		
 		return "/JSP/Checking_Booked.jsp";
 	}
 
