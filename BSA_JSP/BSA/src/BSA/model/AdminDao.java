93b6db10c55249e06b494f57856845461c923c11
kendong18 <kendong18@gmail.com>
2017-11-30 11:37:15 +0900

    bookingdao -> admindao

--- BSA_JSP/BSA/src/BSA/model/AdminDao.java
+++ BSA_JSP/BSA/src/BSA/model/AdminDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminDao {
 	private static AdminDao instance = null;
@@ -43,5 +45,90 @@ public class AdminDao {
 		}
 	}
 	
+	
+	public int getArticleCount() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int count = 0;
+		try {
+			conn = ConnUtil.getConnection();
+			pstmt = conn.prepareStatement("select count(*) from BOOKING");
+			rs= pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}try {
+			if(rs != null)rs.close();
+			if(pstmt != null)pstmt.close();
+			if(conn != null)conn.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+	
+	
+	public List<BookingDto> getArticles(){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BookingDto> articleList = null;
+		try {
+			conn = ConnUtil.getConnection();
+					String sql = "select * from BOOKING";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				articleList = new ArrayList<BookingDto>();
+				do {
+					BookingDto article = new BookingDto();
+					article.setFirst_name(rs.getString("first_name"));
+					article.setLast_name(rs.getString("last_name"));
+					article.setPhone(rs.getString("phone"));
+					article.setEmail(rs.getString("email"));
+					article.setWhere_trip(rs.getString("where_trip"));
+					article.setWhere_from(rs.getString("where_from"));
+					article.setWhere_to(rs.getString("where_to"));
+					article.setPickup(rs.getString("pickup"));
+					article.setDropft(rs.getString("dropft"));
+					article.setStart_airline_name(rs.getString("start_airline_name"));
+					article.setStart_airline_no(rs.getString("start_airline_no"));
+					article.setStart_airline_time(rs.getString("start_airline_time"));
+					article.setArrival_airline_name(rs.getString("arrival_airline_name"));
+					article.setArrival_airline_no(rs.getString("arrival_airline_no"));
+					article.setArrival_airline_time(rs.getString("arrival_airline_time"));
+					article.setBus_time_pickup(rs.getString("bus_time_pickup"));
+					article.setBus_time_dropft(rs.getString("bus_time_dropft"));
+					article.setStart_date(rs.getString("start_date"));
+					article.setArrival_date(rs.getString("arrival_date"));
+					article.setAdults(rs.getString("adults"));
+					article.setYoung(rs.getString("young"));
+					article.setChild(rs.getString("child"));
+					article.setInfatns(rs.getString("infatns"));
+					article.setName_on_card(rs.getString("name_on_card"));
+					article.setCard_number(rs.getString("card_number"));
+					article.setExpiry_year(rs.getString("expiry_year"));
+					article.setExpiry_month(rs.getString("expiry_month"));
+					article.setCsv_number(rs.getString("csv_number"));
+					article.setComm(rs.getString("comm"));
+					article.setMoney(rs.getString("money"));
+					article.setCode(rs.getString("code"));
+					articleList.add(article);
+				}while(rs.next());
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}try {
+			if(rs != null)rs.close();
+			if(pstmt != null)pstmt.close();
+			if(conn != null)conn.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return articleList;
+	}
 	
 }
