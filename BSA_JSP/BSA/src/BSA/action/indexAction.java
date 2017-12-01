9bb5d56c6ae4cdba0ed5ce4af3f890ef1ab6a41e
ghsgkq <ckdgus5820@gmail.com>
2017-12-01 13:30:55 +0900

    fix deletebooking

--- BSA_JSP/BSA/src/BSA/action/indexAction.java
+++ BSA_JSP/BSA/src/BSA/action/indexAction.java
@@ -8,7 +8,7 @@ public class indexAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest req, HttpServletResponse resp) throws Throwable {
-		if(req.getSession().getAttribute("id") == null) {
+		if(req.getSession().getAttribute("id") == null || req.getSession().getAttribute("id").equals("")) {
 			req.getSession().invalidate();
 		}else {
 			String id = (String)req.getSession().getAttribute("id");
