6e4df50cab9052355ccee56d3ba90b5fd2a1374a
josh007 <josh008@naver.com>
2017-12-02 13:59:51 +0900

    gfd
    gdfgdf

--- BSA_JSP/BSA/src/BSA/action/ContentAction.java
+++ BSA_JSP/BSA/src/BSA/action/ContentAction.java
@@ -13,19 +13,7 @@ public class ContentAction implements CommandAction{
 	public String requestPro(HttpServletRequest req, HttpServletResponse resp) throws Throwable {
 		
 		//해당 글 코드
-		String code=req.getParameter("code");
-		
-		
-		//�빐�떦 �럹�씠吏� 踰덊샇
-		
-		AdminDao dbPro = AdminDao.getInstance();
-		
-		//�빐�떦 湲�踰덊샇�뿉 ���븳 �젅肄붾뱶
-		BookingDto article = dbPro.getArticle(code);
-		
-		//酉곗뿉�꽌 �궗�슜�븷 �냽�꽦
-		
-		req.setAttribute("article", req.getParameter("article"));
+	
 		
 		return "/JSP/Content.jsp";
 		
